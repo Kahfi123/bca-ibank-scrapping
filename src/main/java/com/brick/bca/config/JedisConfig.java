e24981efbce8a329412de83cf0196cd9be196f3e
Maulana Kahfi <maulana.kahfi.tif@gmail.com>
2021-03-25 23:50:47 +0700

    Update JedisConfig.java

--- src/main/java/com/brick/bca/config/JedisConfig.java
+++ src/main/java/com/brick/bca/config/JedisConfig.java
@@ -1,38 +1,38 @@
-package com.brick.bca.config;
+// package com.brick.bca.config;
 
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import redis.clients.jedis.JedisPool;
-import redis.clients.jedis.JedisPoolConfig;
+// import org.springframework.beans.factory.annotation.Value;
+// import org.springframework.context.annotation.Bean;
+// import org.springframework.context.annotation.Configuration;
+// import redis.clients.jedis.JedisPool;
+// import redis.clients.jedis.JedisPoolConfig;
 
-@Configuration
-public class JedisConfig {
-    @Value("${spring.redis.host}")
-    private String host;
+// @Configuration
+// public class JedisConfig {
+//     @Value("${spring.redis.host}")
+//     private String host;
 
-    @Value("${spring.redis.port}")
-    private int port;
+//     @Value("${spring.redis.port}")
+//     private int port;
 
-    @Value("${spring.redis.jedis.pool.max-idle}")
-    private int maxIdle;
+//     @Value("${spring.redis.jedis.pool.max-idle}")
+//     private int maxIdle;
 
-    @Value("${spring.redis.jedis.pool.max-wait}")
-    private long maxWait;
+//     @Value("${spring.redis.jedis.pool.max-wait}")
+//     private long maxWait;
 
-    @Value("${spring.redis.jedis.pool.min-idle}")
-    private int minIdle;
+//     @Value("${spring.redis.jedis.pool.min-idle}")
+//     private int minIdle;
 
-    @Value("${spring.redis.timeout}")
-    private int timeout;
+//     @Value("${spring.redis.timeout}")
+//     private int timeout;
 
-    @Bean
-    public JedisPool redisPoolFactory() {
-        JedisPoolConfig jedisPoolConfig = new JedisPoolConfig();
-        jedisPoolConfig.setMaxIdle(maxIdle);
-        jedisPoolConfig.setMaxWaitMillis(maxWait);
-        jedisPoolConfig.setMinIdle(minIdle);
+//     @Bean
+//     public JedisPool redisPoolFactory() {
+//         JedisPoolConfig jedisPoolConfig = new JedisPoolConfig();
+//         jedisPoolConfig.setMaxIdle(maxIdle);
+//         jedisPoolConfig.setMaxWaitMillis(maxWait);
+//         jedisPoolConfig.setMinIdle(minIdle);
 
-        return new JedisPool(jedisPoolConfig, host, port, timeout);
-    }
-}
+//         return new JedisPool(jedisPoolConfig, host, port, timeout);
+//     }
+// }
